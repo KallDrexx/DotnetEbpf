1f668f5488f34041d0288a13dcd0248eb0398c86
KallDrexx <me@mshapiro.net>
2025-01-14 23:01:35 -0500

    closer

--- src/DotnetEbpf/DotnetEbpf.Examples/Uprobe.cs
+++ src/DotnetEbpf/DotnetEbpf.Examples/Uprobe.cs
@@ -5,13 +5,15 @@ namespace DotnetEbpf.Examples;
 
 public static class Uprobe
 {
+    private const string TracingHeaders = "vmlinux.h,<linux/bpf.h>,<bpf/bpf_tracing.h>";
+    
     [WithCAttribute(BpfSections.License)]
     [InitialGlobalValue("\"Dual BSD/GPL\"")]
     [NonPointerString]
     public static string? License;
 
     [WithCAttribute(BpfSections.Uprobe)]
-    [CustomDeclaration("int BPF_KPROBE(uprobe_add, int a, int b)", null, "<bpf/bpf_tracing.h>")]
+    [CustomDeclaration("int BPF_KPROBE(uprobe_add, int a, int b)", null, TracingHeaders)]
     public static int UProbeAdd(int a, int b)
     {
         BpfUtils.Printk("uprobed_add ENTRY: a = %d, b = %d", a, b);
@@ -19,7 +21,7 @@ public static class Uprobe
     }
 
     [WithCAttribute(BpfSections.URetProbe)]
-    [CustomDeclaration("int BPF_KRETPROBE(uretprobe_add, int ret)", null, "<bpf/bpf_tracing.h>")]
+    [CustomDeclaration("int BPF_KRETPROBE(uretprobe_add, int ret)", null, TracingHeaders)]
     public static int URetProbeAdd(int ret)
     {
         BpfUtils.Printk("uprobed_add EXIT: return = %d", ret);
@@ -27,7 +29,7 @@ public static class Uprobe
     }
 
     [WithCAttribute("SEC(\"uprobe//proc/self/exe:uprobed_sub\")")]
-    [CustomDeclaration("int BPF_KPROBE(uprobe_sub, int a, int b)", null, "<bpf/bpf_tracing.h>")]
+    [CustomDeclaration("int BPF_KPROBE(uprobe_sub, int a, int b)", null, TracingHeaders)]
     public static int UProbeSub(int a, int b)
     {
         BpfUtils.Printk("uprobed_sub ENTRY: a = %d, b = %d", a, b);
@@ -35,7 +37,7 @@ public static class Uprobe
     }
 
     [WithCAttribute("SEC(\"uretprobe//proc/self/exe:uprobed_sub\")")]
-    [CustomDeclaration("int BPF_KRETPROBE(uretprobe_sub, int ret)", null, "<bpf/bpf_tracing.h>")]
+    [CustomDeclaration("int BPF_KRETPROBE(uretprobe_sub, int ret)", null, TracingHeaders)]
     public static int URetProbeSub(int ret)
     {
         BpfUtils.Printk("uprobed_sub EXIT: return = %d", ret);
